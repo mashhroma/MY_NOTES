f46843eb5a57037c65840d99eb55e6cb61052bbd
Mashhroma <mashh@mail.ru>
2023-06-23 11:07:59 +0300

    version without filters

---
+++ controller.py
@@ -0,0 +1,75 @@
+from time import sleep
+import model
+import view
+import commands
+
+notebook = model.Notebook()
+path_exist = False
+
+view.show_greeding()
+
+if input('Введите 1 или 2: ') == '2':
+    path = input('Загрузите файл с заметками в директорию и напишите его название: ')
+    notebook.load_notebook(path)
+    path_exist = True
+    print('>>> Заметки загружены в программу.\n')
+
+sleep(1)
+
+view.show_menu(commands.text_menu)
+
+while True:
+    choice = input('ВВЕДИТЕ КОМАНДУ (подсказка - help): ')
+    match choice:
+        case '1' | 'add': 
+            add = view.get_new_info()
+            notebook.add_note(add)
+            print('>>> Заметка добавлена.')
+
+        case '2' | 'find':
+            search = notebook.find_note(input('Введите данные для поиска: ')).lower()
+            print('>>> Результаты поиска:')
+            print(search)
+
+        case '3' | 'edit': 
+            index = int(input('Введите номер заметки: '))
+            if notebook.check_index(index):
+                edit_info = view.get_new_info()
+                notebook.edit_note(index, edit_info)
+                print('>>> Заметка изменена.')
+            else:
+                print('Заметки с таким номером нет.')
+
+        case '4' | 'del': 
+            index = int(input('Введите номер заметки: '))
+            if notebook.check_index(index):
+                notebook.del_note(index)
+                print('>>> Заметка удалена.')
+            else:
+                print('Заметки с таким номером нет.')
+
+        case '5' | 'show by data': 
+            date = notebook.find_note(input('Введите дату: '))
+            print(f'>>> Заметки на дату: {date}:')
+            print(search)
+
+        case '6' | 'show': 
+            print(notebook)
+
+        case '7' | 'save' : 
+            if path_exist == False:
+                file_name = input('Укажите наименование файла заметок: ')
+                path = f'{file_name}.json'
+                path_exist = True
+            notebook.save_notebook(path)
+            print(f'>>> Заметки сохранены в файл {path}.')
+
+        case '8' | 'help' : 
+            view.show_menu(commands.text_menu)
+
+        case '9' | 'exit':
+            print('До встречи!')
+            exit()
+
+        case _:
+            print('ЭТО НЕ КОМАНДА!')
---
+++ view.py
@@ -0,0 +1,26 @@
+def show_greeding():
+    print('''Добрый день!
+Данная программа поможет Вам создавать заметки и управлять ими.
+Выберите действие:
+1. Создать новую записную книжку
+2. Загрузить записную книжку из файла (json)''')
+    
+def show_menu(text_menu: str):
+    menu = text_menu.split('\n')
+    for i, item in enumerate(menu):
+        if i == 0:
+            print(item)
+        else:
+            print(i, item)
+   
+def get_new_info():
+    date = input('Укажите дату в формате: 01.01.2000: ').split('.')
+    if len(date)<=2:
+        date.append('2023')
+    else:
+        if len(date[2]) < 4:
+            date[2] ='20' + date[2]
+    date = '.'.join(date)
+    title = input('Название заметки: ')
+    comment = input('Комментарии к заметке: ')
+    return {'date': date, 'title': title, 'comment': comment}
